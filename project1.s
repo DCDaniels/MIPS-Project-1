bceee859ea872034282aa9f41a546cddcc872f75
David <davidcharlesdaniels@gmail.com>
2019-10-30 16:01:11 -0400

    Get and limit user input

--- project1.s
+++ project1.s
@@ -3,5 +3,9 @@ InputVariable: .space  #Variable for user input
 
 .globl main
 main:
+	li $v0, 8				#Allows user to input
+	la $a0, InputVariable			#Saves input to  variable
+	li $a1,11 				#Limits the input to 10
+	syscall 				#Issues a System Call
 	
 
