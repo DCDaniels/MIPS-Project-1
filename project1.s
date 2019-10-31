55983b36fe79ff3db5819a9256beff0e4216deb1
David <davidcharlesdaniels@gmail.com>
2019-10-30 23:58:43 -0400

    Adjusting loop

--- project1.s
+++ project1.s
@@ -1,8 +1,8 @@
 .data 						#Declarations
 InputVariable: .space 10			#Variable for user input 
 
-.text
-.globl main
+.text						#Instructions stored in text segment at next available address
+.globl main					#Allows main to be refrenced anywhere
 
 main:
 	li $v0, 8				#Allows user to input
@@ -10,13 +10,17 @@ main:
 	li $a1,11 				#Limits the input to 10
 	syscall 				#Issues a System Call
 	
-	la $t1, InputVariable			#Load the variable to the register $t1
+	la $t1,InputVariable			#Load the variable to the register $t1
 	li $t2,0				#Initialize $t2 for sum
 	li $t3,32				#Stored a space in $t3 to check for spaces
-	li $t4, 0x0A				#Created a new line here 
+	li $t4,0x0A				#Created a new line here 
 	
 loop: 
-	lb $t5,0($t1)				#Put the values into $t5
+	lb $t5,0($t1)				#Puts each character into $t5
+	beq $t5,  	
+	
+	
+	
 	 			
 	
 
