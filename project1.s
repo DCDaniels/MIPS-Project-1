59605cc594774432bd406144d160e9b37baf970c
David <davidcharlesdaniels@gmail.com>
2019-10-31 00:39:11 -0400

    Checks for bad characters in loop and initialized a register to 0

--- project1.s
+++ project1.s
@@ -13,11 +13,16 @@ main:
 	la $t1,InputVariable			#Load the variable to the register $t1
 	li $t2,0				#Initialize $t2 for sum
 	li $t3,32				#Stored a space in $t3 to check for spaces
+	li $t6,0				#Initialized a register to 0
 	li $t4,0x0A				#Created a new line here 
 	
-loop: 
+loop: 						#Loops through all letters in string
 	lb $t5,0($t1)				#Puts each character into $t5
-	beq $t5,  	
+	beq $t5,$t4, loop_ending		#End Loop when all characters are used
+	beq $t5,$t3, ignore_char		#If a character is a space it will skip
+	beq $t5,$t6, ignore_char		#If a character is 0 it will skip
+	jr $ra					#Jump Register unconditionally
+	  	
 	
 	
 	
