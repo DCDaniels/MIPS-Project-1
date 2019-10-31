1b982bf8ad91541e83e8b9196c774ecbd8a896ba
David <davidcharlesdaniels@gmail.com>
2019-10-31 19:03:14 -0400

    Made new registers and started numbers function

--- project1.s
+++ project1.s
@@ -1,3 +1,7 @@
+#HU ID: @02884990
+#02884990%11 = 9
+#26 + 9 = Base 35
+#Range (0,y) and (0-Y)
 .data 						#Declarations
 InputVariable: .space 10			#Variable for user input 
 
@@ -7,19 +11,22 @@ InputVariable: .space 10			#Variable for user input
 main:
 	li $v0, 8				#Allows user to input
 	la $a0, InputVariable			#Saves input to  variable
-	li $a1,11 				#Limits the input to 10
+	li $a1,12 				#Limits the input to 10
 	syscall 				#Issues a System Call
 	
 	la $t1,InputVariable			#Load the variable to the register $t1
 	li $t2,0				#Initialize $t2 to calculate sum
 	li $t3,32				#Stored a space in $t3 to check for spaces
-	li $t6,0				#Initialized a register to 0
+	li $t6,48				#Initialized a register to 0
 	li $t4,0x0A				#Created a new line here 
 	li $s1, 64				#Beginning of Capital Letters for ASCII
 	li $s2, 89				#Ending of Capital Letters for ASCII
 	li $s3, 97				#Beginning of Common Letters for ASCII
 	li $s4, 122				#Ending of Common Letters for ASCII
 	li $s5, 0				#Placeholder for calculations
+	li $s6, 65				#Need in order to find numbers less than 65 without ignoring 64
+	li $s7, 57				#Ending of numbers
+	
 	
 loop: 						#Loops through all letters in string
 	lb $t5,0($t1)				#Puts each character into $t5
@@ -27,6 +34,7 @@ loop: 						#Loops through all letters in string
 	beq $t5,$t3, ignore_char		#If a character is a space it will skip
 	beq $t5,$t6, ignore_char		#If a character is 0 it will skip
 	bgt $t5,$s1, capital_letters		#If character > 65 go to capital_letters
+	blt $t5,$s6, numbers			#If character < 64
 	addi $t1,$t1, 1				#Increment to check next character in the loop
 	j loop					#Restart the loop
 
@@ -57,6 +65,11 @@ common_letters:					#Function that makes sure to check for common letters
 	bgt $t5,$s4, ignore_char		#Branch if in greater than 122
 	addi $s5,$t5,-87			#Stores the decimal value of common letters in $s5
 	add $t2,$t2,$s5				#Add the character to the sum and store in $t2
+	
+numbers:
+	bgt $t5,$s7,ignore_char			#Ignore characters after the numbers
+		
+	
 
 
 
